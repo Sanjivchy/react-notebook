97c43191f7cd7a3281cddbd27e20cebbbfa99331
sharad9201 <sharad.rae19@gmail.com>
2021-07-20 22:25:00 +0545

    just a profile image

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Sidebar.css';
 import {SidebarData} from './SidebarData';
+import Img1 from '../pages/home/images/img1.png';
+
+
+
 
 function Sidebar() {
     return (
         <div className="Bar"> 
+         <div className="self-center w-7/12 max-w-2xl row-start-1 ml-4 md:w-8/10 justify-self-center md:row-end-1">
+                          <img src={Img1} alt="iamges"/>
+                         </div>
             <ul className="SidebarList">
                 {SidebarData.map((val, key ) => { 
 
@@ -13,6 +20,7 @@ function Sidebar() {
                         id={window.location.pathname == val.link ? "active" : ""}
                         onClick={()=> {window.location.pathname = val.link}}>
                         {" "} 
+                        
                         <div id="icon">  {val.icon} </div>
                         <div id="title"> {val.title} </div>
                         <div className="sideline"></div> 
